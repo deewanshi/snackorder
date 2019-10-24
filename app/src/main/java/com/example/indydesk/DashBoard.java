02a2ef51eeefff40b7837f0f9ea2f40cbbc4590d
Tanvigupta96 <guptatannu.34gmail.com>
2019-10-24 23:44:15 +0530

    all changes done

--- app/src/main/java/com/example/indydesk/DashBoard.java
+++ app/src/main/java/com/example/indydesk/DashBoard.java
@@ -1,4 +1,5 @@
 package com.example.indydesk;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Rect;
@@ -49,7 +50,23 @@ public class DashBoard extends AppCompatActivity {
                     AlertDialog.Builder builder=new AlertDialog.Builder(DashBoard.this);
                     builder.setTitle("Workout Warrior Half Sleeve T-Shirt");
                     builder.setIcon(R.drawable.album1);
-                    builder.setMessage("Fifbi Apparela Rs.399.0");
+                    builder.setMessage("Fifbi Apparela\n A very stylist T shirt. Perfect for layering under blazers. shirts or smart enough to wear on its own. Perfect fit and soft cotton. more then sufficient to beat this summer heat with style.\n" +
+                            "\n" +
+                            "Fit Type: Regular Fit\n" +
+                            "Brand: Fifbi\n" +
+                            "Sleeve: Half Sleeve\n" +
+                            "Neck: Round Neck\n" +
+                            "Fitting: Regular Fit\n" +
+                            "Material: Cotton \n ");
+
+
+                    builder.setNegativeButton("Rs.399.0", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+
 
                     AlertDialog dialog = builder.create();
                     dialog.show();
@@ -60,9 +77,25 @@ public class DashBoard extends AppCompatActivity {
                 else if(album.getThumbnail()==R.drawable.album2){
 
                     AlertDialog.Builder builder=new AlertDialog.Builder(DashBoard.this);
-                    builder.setTitle("Student Waldrof Half Sleeve T-Shirt");
+                    builder.setTitle("Studio Waldrof Half Sleeve T-Shirt");
                     builder.setIcon(R.drawable.album2);
-                    builder.setMessage("Fifbi Apparela Rs.399.0");
+                    builder.setMessage("Fifbi Apparela\n A very stylist T shirt. Perfect for layering under blazers. shirts or smart enough to wear on its own. Perfect fit and soft cotton. more then sufficient to beat this summer heat with style.\n" +
+                            "\n" +
+                            "Fit Type: Regular Fit\n" +
+                            "Brand: Fifbi\n" +
+                            "Sleeve: Half Sleeve\n" +
+                            "Neck: Round Neck\n" +
+                            "Fitting: Regular Fit\n" +
+                            "Material: Cotton \n ");
+
+
+
+                    builder.setNegativeButton("Rs.399.0", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
 
                     AlertDialog dialog = builder.create();
                     dialog.show();
@@ -73,7 +106,21 @@ public class DashBoard extends AppCompatActivity {
                     AlertDialog.Builder builder=new AlertDialog.Builder(DashBoard.this);
                     builder.setTitle("Stand up for what is Right Half Sleeve T-Shirt");
                     builder.setIcon(R.drawable.album3);
-                    builder.setMessage("Fifbi Apparela Rs.399.0");
+                    builder.setMessage("Fifbi Apparela\n A very stylist T shirt. Perfect for layering under blazers. shirts or smart enough to wear on its own. Perfect fit and soft cotton. more then sufficient to beat this summer heat with style.\n" +
+                            "\n" +
+                            "Fit Type: Regular Fit\n" +
+                            "Brand: Fifbi\n" +
+                            "Sleeve: Half Sleeve\n" +
+                            "Neck: Round Neck\n" +
+                            "Fitting: Regular Fit\n" +
+                            "Material: Cotton \n ");
+
+                    builder.setNegativeButton("Rs.399.0", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
 
                     AlertDialog dialog = builder.create();
                     dialog.show();
@@ -84,7 +131,21 @@ public class DashBoard extends AppCompatActivity {
                     AlertDialog.Builder builder=new AlertDialog.Builder(DashBoard.this);
                     builder.setTitle("Daddy! Half Sleeve T-Shirt");
                     builder.setIcon(R.drawable.album4);
-                    builder.setMessage("Fifbi Apparela Rs.399.0");
+                    builder.setMessage("Fifbi Apparela\n A very stylist T shirt. Perfect for layering under blazers. shirts or smart enough to wear on its own. Perfect fit and soft cotton. more then sufficient to beat this summer heat with style.\n" +
+                            "\n" +
+                            "\n" +
+                            "Fit Type: Regular Fit\n" +
+                            "Brand: Fifbi\n" +
+                            "Sleeve: Half Sleeve\n" +
+                            "Neck: Round Neck\n" +
+                            "Fitting: Regular Fit\n" +
+                            "Material: Cotton \n ");
+
+                    builder.setNegativeButton("Rs.399.0", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    });
 
                     AlertDialog dialog = builder.create();
                     dialog.show();
@@ -95,7 +156,19 @@ public class DashBoard extends AppCompatActivity {
                     AlertDialog.Builder builder=new AlertDialog.Builder(DashBoard.this);
                     builder.setTitle("Men's Premium Leather TimberLand Belt with classic buckle");
                     builder.setIcon(R.drawable.album5);
-                    builder.setMessage("Fitbi  Rs.750.0");
+                    builder.setMessage("Fifbi\n Fifbi Brought Up This Brown Coloured Single Pack Belt That Will Add A Touch Of Understated Style To Your Overall Office Appearance. Made Of Rich Quality Faux Leather, This Belt For Men Is Best Worn With Casual Or Semi-Casual. This Belt Will Not Only Keep Your Trousers And Jeans In Place But Will Also Make You Look Stylish. Material Of This Belt Is Of Faux Leather Which Is Highest In Its Quality\n" +
+                            "\n" +
+                            "\n" +
+                            "Material:- 100% Genuine leather\n" +
+                            "Pure Leather Brown Belt\n" +
+                            "QUALITY: - Made of Artificial leather (PU Leather) Belt. *MADE IN INDIA. Specially designed for those who do not like animals leather product.\n" +
+                            "CUT TO FIT STYLE:- Cut the belt as per your waist size and wear it. \n ");
+
+                    builder.setNegativeButton("Rs.750.00", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    });
 
                     AlertDialog dialog = builder.create();
                     dialog.show();
@@ -106,7 +179,21 @@ public class DashBoard extends AppCompatActivity {
                     AlertDialog.Builder builder=new AlertDialog.Builder(DashBoard.this);
                     builder.setTitle("Men's Reversible Premium Leather Blt with Removable buckle");
                     builder.setIcon(R.drawable.album6);
-                    builder.setMessage("Fitbi  Rs.1200.0");
+                    builder.setMessage("Fifbi\n TWO IN ONE:- Reversible belt, Twist buckle to change belt side. REVERSIBLE: It's a Reversible belt. just pull and turnbuckle and use it from BOTH SIDE BLACK AND BROWN. Quality: Premium. Made In India But Material Imported. A History & Tradition of Exceptional Quality: This Premium ADDITION full grain leather belt construction offers the highest quality and maximum durability. A Black shade which goes well with any of your formal or evening wear. This comes with a premium chrome buckle. **Note:- The Buckle design may slightly vary as per the availability. CARE INSTRUCTION - Clean with the SOFT cotton cloth.\n" +
+                            "\n" +
+                            "\n" +
+                            "\n" +
+                            "Good Quality Italian Leather\n" +
+                            "Made In India.\n" +
+                            "MAKE SURE TO ORDER 2 SIZE BIGGER THAN YOUR WAIST/PANTS SIZE” as a rule of thumb your belt size needs to be approx. If your waist size is 28-inch, order belt of 32-inch. If your waist size is 30-inch, order belt of 34-inch and so on.\n" +
+                            "Fifbi's black and brown leather-crafted belts are constructed from High Quality 100% genuine leather, which ensures the high quality and durability of the belt.\n" +
+                            "Our customers love our products and make repetitive purchases. Most orders are dispatched within 24 hours giving you faster delivery. Just try our products and world-class services. \n ");
+
+                    builder.setNegativeButton("Rs.1200.0", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    });
 
                     AlertDialog dialog = builder.create();
                     dialog.show();
@@ -117,7 +204,16 @@ public class DashBoard extends AppCompatActivity {
                     AlertDialog.Builder builder=new AlertDialog.Builder(DashBoard.this);
                     builder.setTitle("Men's Classic Buckle belt with Premium Leather");
                     builder.setIcon(R.drawable.album7);
-                    builder.setMessage("Fitbi  Rs.1200.0");
+                    builder.setMessage("Fifbi\n Make Sure To Order 2 Size Bigger Than Your Waist/Pants Size\" as a rule of thumb your belt size needs to be approx. If your waist size is 28-inch, order belt of 32-inch. If your waist size is 30-inch, order belt of 34-inch and so on. For any query, ping us on messenger. \n 100% Pure Genuine Leather Belt. Handmade, Highly constructed for longer endurance and better upholding.\n   \n ");
+
+
+                    builder.setNegativeButton("Rs.1200.0", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    });
+
+
 
                     AlertDialog dialog = builder.create();
                     dialog.show();
@@ -128,7 +224,24 @@ public class DashBoard extends AppCompatActivity {
                     AlertDialog.Builder builder=new AlertDialog.Builder(DashBoard.this);
                     builder.setTitle("Men's Premium Leather Single Prong Classic Bucle belt");
                     builder.setIcon(R.drawable.album8);
-                    builder.setMessage("Fitbi Rs.650.0");
+                    builder.setMessage("Fifbi\n Customer's satisfaction is our mission- we are here to provide our esteemed customers a most pleasant experience. Our 30 days free return policy enables you a risk-free purchase! For any reason, you are unsatisfied with our belt, full refund or free replace is available as per your wish.\n" +
+                            "\n" +
+                            " \n" +
+                            "\n" +
+                            "Pack of 1. Width: 35 mm. Material : Genuine Leather. Occasion: Formal & Casual Wear.\n" +
+                            "Quality: Premium. Made In India. Made In India.\n" +
+                            "MAKE SURE TO ORDER 2 SIZE BIGGER THAN YOUR WAIST/PANTS SIZE\" as a rule of thumb your belt size needs to be approx. If your waist size is 28-inch, order belt of 32-inch. If your waist size is 30-inch, order belt of 34-inch and so on.\n" +
+                            "Fifbi's brown leather-crafted belts are constructed from High Quality 100% genuine leather, which ensures the high quality and durability of the belt.\n" +
+                            "Our customers love our products and make repetitive purchases. Most orders are dispatched within 24 hours giving you a faster delivery. Just try our products and world class services. \n ");
+
+
+                    builder.setNegativeButton("Rs.650.0", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+
 
                     AlertDialog dialog = builder.create();
                     dialog.show();
@@ -139,7 +252,18 @@ public class DashBoard extends AppCompatActivity {
                     AlertDialog.Builder builder=new AlertDialog.Builder(DashBoard.this);
                     builder.setTitle("Men's Genuine Leather Belt with Fashionable & Classic Buckle");
                     builder.setIcon(R.drawable.album9);
-                    builder.setMessage("Fitbi  Rs.700.0");
+                    builder.setMessage("Fitbi\n  Fifbi presents you one of the most versatile and durable belts which are perfectly suitable to go with many outfit options. You can style it with jeans as well as trousers.\n" +
+                            "They are a perfect value for money product and are crafted with pure leather but at a cost-effective price.\n" +
+                            "The belts are handmade in India from soft brown/black leather and with a high-quality buckle.\n" +
+                            "Perfectly suitable to use yourself or to gift someone you love. \n ");
+
+
+                    builder.setNegativeButton("Rs.700.0", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    });
+
 
                     AlertDialog dialog = builder.create();
                     dialog.show();
@@ -150,7 +274,20 @@ public class DashBoard extends AppCompatActivity {
                     AlertDialog.Builder builder=new AlertDialog.Builder(DashBoard.this);
                     builder.setTitle("Men's Slim Genuine smooth Leather Money Clip Dark Brown Wallet");
                     builder.setIcon(R.drawable.album10);
-                    builder.setMessage("Fifbi  Rs.500.0");
+                    builder.setMessage("Fifbi\n The Best Pure Leather Wallets For Men! Is Your Existing Wallet Needed To Be Replaced? Do You Want To Have The Best Pure Leather Wallet On Your Cart? You Have Come To The Right Place! When It Comes To Best Wallet Brands For Men,We Have Made Men'S Wallets That Are Available Your Needs.\n" +
+                            "\n" +
+                            "100% Pure Leather\n" +
+                            "Made In India\n" +
+                            "It's Not Bulky: This Slim Wallet Will Never Become Bulky Even After You Keep All Your Atm Cards, Credit Cards And Cash\n" +
+                            "This Perfect Sized Leather Wallet Will Seamlessly Slide Into Your Pocket., Soft & Firm Leather:\n" +
+                            "This Men's Wallet Is Neatly Designed And Made With Soft And Firm Genuine Leather And Twill Fabric On The Inside. You Will Feel The Softness On Your Hand And It Will Never Be A Hard Leather Product \n ");
+
+                    builder.setNegativeButton("Rs.500.0", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    });
+
 
                     AlertDialog dialog = builder.create();
                     dialog.show();
@@ -161,7 +298,21 @@ public class DashBoard extends AppCompatActivity {
                     AlertDialog.Builder builder=new AlertDialog.Builder(DashBoard.this);
                     builder.setTitle("Men's Slim Genuine smooth Leather Money Clip Light Brown Wallet");
                     builder.setIcon(R.drawable.album11);
-                    builder.setMessage("Fitbi  Rs.500.0");
+                    builder.setMessage("Fifbi\n The Best Pure Leather Wallets For Men! Is Your Existing Wallet Needed To Be Replaced? Do You Want To Have The Best Pure Leather Wallet On Your Cart? You Have Come To The Right Place! When It Comes To Best Wallet Brands For Men,We Have Made Men'S Wallets That Are Available Your Needs.\n" +
+                            "\n" +
+                            "100% Pure Leather\n" +
+                            "Made In India\n" +
+                            "It's Not Bulky: This Slim Wallet Will Never Become Bulky Even After You Keep All Your Atm Cards, Credit Cards And Cash\n" +
+                            "This Perfect Sized Leather Wallet Will Seamlessly Slide Into Your Pocket., Soft & Firm Leather:\n" +
+                            "This Men's Wallet Is Neatly Designed And Made With Soft And Firm Genuine Leather And Twill Fabric On The Inside. You Will Feel The Softness On Your Hand And It Will Never Be A Hard Leather Product \n ");
+
+
+                    builder.setNegativeButton("Rs.500.0", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    });
+
 
                     AlertDialog dialog = builder.create();
                     dialog.show();
